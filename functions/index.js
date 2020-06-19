ec14cf8d3722f99b84782497504f2297ffd78ca8
Gruenfelder <maximilian.gruenfelder@sap.com>
2020-06-19 13:05:05 +0200

    WIP notifications alexa skill

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,34 @@
-const functions = require("firebase-functions");
+'use strict'
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// })
+const functions = require("firebase-functions");
+const admin = require("firebase-admin");
+const cors = require('cors');
+const express = require('express');
+const app = express();
+
+const { requestOAuthAccessToken, triggerNotification } = require("./src/alexa-skill");
+
+//Router files
+// const userRouter = require("./router/user");
+// const testResultRouter = require("./router/testResult");
+
+admin.initializeApp();
+
+app.use(cors({ origin: true }));
+
+app.get("/test", async (req, res) => {
+    // try {
+    //     await triggerNotification(undefined, await requestOAuthAccessToken())
+    //     res.send(200);
+    // } catch(err) {
+    //     console.log("error happened")
+    //     console.error(err);
+    //     console.error(err.data);
+    //     res.send(err);
+    // }
+})
+
+// app.use("/user", userRouter);
+// app.use("/testResult", testResultRouter);
+
+exports.api = functions.https.onRequest(app);
---
+++ functions/src/alexa-skill.js
@@ -0,0 +1,89 @@
+'use strict'
+
+const axios = require("axios");
+const { uuid } = require('uuidv4');
+
+const requestOAuthAccessToken = async () => {
+    const clientId = "amzn1.application-oa2-client.2f90a17556b34e04ad44d9776d713c7a";
+    const clientSecret = "cd733983bf86f0a4e626953f2e2d0f48e2f65c845d7c65aebc007ed4d7426016";
+    const url = `https://api.amazon.com/auth/O2/token`;
+    const data = `grant_type=client_credentials&client_id=${clientId}&client_secret=${clientSecret}&scope=alexa::proactive_events`;
+    const config = {
+        url,
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/x-www-form-urlencoded',
+        },
+        data,
+    };
+    try {
+        const response = await axios(config);
+        return response.data.access_token;
+    }
+    catch (err) {
+        console.error(err);
+        return err;
+    }
+}
+
+const triggerNotification = async (proactiveEvent, accessToken, live = false) => {
+    if (!accessToken) {
+        throw new Error("Can't find accessToken");
+    }
+    const url = `https://api.eu.amazonalexa.com${live ? '/v1/proactiveEvents' : '/v1/proactiveEvents/stages/development'}`;
+    const config = {
+        data: getNotificationTemplate(),
+        url,
+        method: "POST",
+        headers: {
+            'Content-Type': 'application/json',
+            'Authorization': `Bearer ${accessToken}`,
+        },
+    };
+
+    const result = await axios(config);
+    return result;
+}
+
+
+const getNotificationTemplate = () => {
+    // Sets timestamp to current date and time
+    let timestamp = new Date();
+    timestamp = timestamp.toISOString();
+
+    // Sets expiryTime 23 hours ahead of the current date and time
+    let expiryTime = new Date();
+    expiryTime.setHours(expiryTime.getHours() + 23);
+    expiryTime = expiryTime.toISOString();
+    return {
+        timestamp: timestamp,
+        referenceId: uuid(),
+        expiryTime: expiryTime,
+        event: {
+            name: "AMAZON.MessageAlert.Activated",
+            payload: {
+                state: {
+                    status: "UNREAD",
+                    freshness: "NEW"
+                },
+                messageGroup: {
+                    creator: {
+                        name: "einem unbearbeiteten Learning Agreement von Christian Axtmann"
+                    },
+                    count: 1
+                },
+            },
+        },
+        localizedAttributes: [],
+        relevantAudience: {
+            type: "Multicast",
+            payload: {
+            }
+        }
+    };
+}
+
+module.exports = {
+    requestOAuthAccessToken,
+    triggerNotification
+}
