bd66c2e895d9ea7f7af6076aba0e01572b3fcd90
Gruenfelder <maximilian.gruenfelder@sap.com>
2020-06-28 21:47:29 +0200

    id function for firestore documents

--- functions/util/helper.js
+++ functions/util/helper.js
@@ -5,10 +5,26 @@ const isLocalFunctions = process.env.NODE_ENV === "functions";
 const isLocal = isLocalFireStore || isLocalFunctions;
 let importRunningState = false;
 
+const generateFirestoreDocumentID = () => {
+  const chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  let autoId = '';
+  while (autoId.length < 20) {
+    const bytes = crypto_1.randomBytes(40);
+    bytes.forEach(b => {
+      const maxValue = 62 * 4 - 1;
+      if (autoId.length < 20 && b <= maxValue) {
+        autoId += chars.charAt(b % 62);
+      }
+    });
+  }
+  return autoId;
+};
+
 module.exports = {
   isLocal,
   isLocalFireStore,
   isLocalFunctions,
   setImportRunningState: (value) => (importRunningState = value),
   getImportRunningState: () => importRunningState,
+  generateFirestoreDocumentID
 };
