e8ccbb29b5df0b4e87e3db37bc6d5c35b27079b4
Max <max.grunfelder@gmail.com>
2020-06-29 10:16:13 +0200

    Generate ID for mock data (#22)
    * id function for firestore documents
    
    * auto replace key field
    
    * cleanup collection before
    
    * auto generate id for documents
    
    Co-authored-by: Gruenfelder <maximilian.gruenfelder@sap.com>

--- functions/util/dataImport.js
+++ functions/util/dataImport.js
@@ -1,11 +1,16 @@
 const mockData = require("../data/mockData.json");
 const admin = require("firebase-admin");
+const { generateFirestoreDocumentID } = require("./helper");
+const { firestore } = require("firebase-admin");
+let idMap = {};
 
 const importData = async () => {
   const db = admin.firestore();
   const dbPromises = [];
   try {
-    Object.keys(mockData).forEach((collection) => {
+    replaceKeyFieldsInCollectionAndDocuments(newIdMap(), mockData);
+    Object.keys(mockData).forEach(async (collection) => {
+      await deleteCollection(collection);
       const currentCollection = mockData[collection];
       Object.keys(currentCollection).forEach((document) => {
         const currentDocument = currentCollection[document];
@@ -36,6 +41,65 @@ const importData = async () => {
     console.error("Import Failed");
     throw new Error(err);
   }
+};
+
+const newIdMap = () => {
+  const idMapFunction = (id) => {
+    if (Object.prototype.hasOwnProperty.call(idMap, id)) {
+      return idMap[id];
+    } else {
+      idMap[id] = generateFirestoreDocumentID();
+      return idMap[id];
+    }
+  };
+  return idMapFunction;
+};
+
+const replaceKeyFieldsInCollectionAndDocuments = (idMap, mockData) => {
+  Object.keys(mockData).forEach((collection) => {
+    Object.keys(mockData[collection]).forEach((document) => {
+      if (checkIfKeyShouldBeReplaced(document)) {
+        const newID = idMap(document);
+        mockData[collection][newID] = mockData[collection][document];
+        delete mockData[collection][document];
+        replaceDocumentDeep(idMap, mockData[collection][newID]);
+      } else {
+        replaceDocumentDeep(idMap, mockData[collection][document]);
+      }
+    });
+  });
+};
+
+const replaceDocumentDeep = (idMap, document) => {
+  if (Array.isArray(document)) {
+    document.forEach((element) => {
+      replaceDocumentDeep(idMap, element);
+    });
+    return;
+  }
+  Object.keys(document).forEach((field) => {
+    // eslint-disable-next-line valid-typeof
+    if (typeof document[field] === "object" && typeof document[field] !== null) {
+      return replaceDocumentDeep(idMap, document[field]);
+    }
+    if (checkIfKeyShouldBeReplaced(document[field])) {
+      document[field] = idMap(document[field]);
+    }
+  });
+};
+
+const checkIfKeyShouldBeReplaced = (key) => {
+  return key !== null && typeof key === "string" && key.startsWith("id_");
+};
+
+const deleteCollection = async (collection) => {
+  const db = admin.firestore();
+  const batch = db.batch();
+  const snapshot = await db.collection(collection).orderBy("__name__").get();
+  snapshot.docs.forEach((doc) => {
+    batch.delete(doc.ref);
+  });
+  await batch.commit();
 };
 
 module.exports = {
