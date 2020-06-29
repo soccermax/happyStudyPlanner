9bc5fe8e10d4ace4a332209e36c005b02ede675b
Gruenfelder <maximilian.gruenfelder@sap.com>
2020-06-29 09:56:43 +0200

    auto replace key field

--- functions/util/dataImport.js
+++ functions/util/dataImport.js
@@ -1,5 +1,7 @@
 const mockData = require("../data/mockData.json");
 const admin = require("firebase-admin");
+const { generateFirestoreDocumentID } = require("./helper");
+let idMap = {};
 
 const importData = async () => {
   const db = admin.firestore();
@@ -7,6 +9,7 @@ const importData = async () => {
   try {
     Object.keys(mockData).forEach((collection) => {
       const currentCollection = mockData[collection];
+      replaceKeyFieldsInCollectionAndDocuments(newIdMap(), mockData, currentCollection);
       Object.keys(currentCollection).forEach((document) => {
         const currentDocument = currentCollection[document];
         if (currentDocument.lastEvaluatedOn) {
@@ -36,6 +39,53 @@ const importData = async () => {
     console.error("Import Failed");
     throw new Error(err);
   }
+};
+
+const newIdMap = () => {
+  const idMapFunction = (id) => {
+    if (Object.prototype.hasOwnProperty.call(idMap, id)) {
+      return idMap[id];
+    } else {
+      idMap[id] = generateFirestoreDocumentID();
+      return idMap[id];
+    }
+  };
+  return idMapFunction;
+};
+
+const replaceKeyFieldsInCollectionAndDocuments = (idMap, mockData, currentCollection) => {
+  Object.keys(currentCollection).forEach((document) => {
+    if (checkIfKeyShouldBeReplaced(document)) {
+      const newID = idMap(document);
+      currentCollection[newID] = currentCollection[document];
+      delete currentCollection[document];
+      replaceDocumentDeep(idMap, currentCollection[newID]);
+    } else {
+      replaceDocumentDeep(idMap, currentCollection[document]);
+    }
+  });
+};
+
+const replaceDocumentDeep = (idMap, document) => {
+  if (Array.isArray(document)) {
+    document.forEach((element) => {
+      replaceDocumentDeep(idMap, element);
+    });
+    return;
+  }
+  Object.keys(document).forEach((field) => {
+    if (typeof document[field] === "object" && typeof document[field] !== null) {
+      // eslint-disable-line valid-typeof
+      return replaceDocumentDeep(idMap, document[field]);
+    }
+    if (checkIfKeyShouldBeReplaced(document[field])) {
+      document[field] = idMap(document[field]);
+    }
+  });
+};
+
+const checkIfKeyShouldBeReplaced = (key) => {
+  return key !== null && typeof key === "string" && key.startsWith("id_");
 };
 
 module.exports = {
--- functions/util/helper.js
+++ functions/util/helper.js
@@ -1,16 +1,18 @@
 "use strict";
 
+const crypto = require("crypto");
+
 const isLocalFireStore = process.env.NODE_ENV === "firestore";
 const isLocalFunctions = process.env.NODE_ENV === "functions";
 const isLocal = isLocalFireStore || isLocalFunctions;
 let importRunningState = false;
 
 const generateFirestoreDocumentID = () => {
-  const chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-  let autoId = '';
+  const chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+  let autoId = "";
   while (autoId.length < 20) {
-    const bytes = crypto_1.randomBytes(40);
-    bytes.forEach(b => {
+    const bytes = crypto.randomBytes(40);
+    bytes.forEach((b) => {
       const maxValue = 62 * 4 - 1;
       if (autoId.length < 20 && b <= maxValue) {
         autoId += chars.charAt(b % 62);
@@ -26,5 +28,5 @@ module.exports = {
   isLocalFunctions,
   setImportRunningState: (value) => (importRunningState = value),
   getImportRunningState: () => importRunningState,
-  generateFirestoreDocumentID
+  generateFirestoreDocumentID,
 };
